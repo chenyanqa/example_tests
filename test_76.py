bc8b8f410e430987aeab1febdb0a6bd56c473a4b
chenyanqa <879167367@qq.com>
2017-12-01 14:12:29 +0800

    add 76

--- test_76.py
+++ test_76.py
@@ -3,22 +3,29 @@
 
 #编写一个函数，输入n为偶数时，调用函数求1/2+1/4+...+1/n,当输入n为奇数时，调用函数1/1+1/3+...+1/n
 def test_oushu(n):
+    i = 0
+    s = 0.0
+    for i in range(2,n+1,2):   #初始值为2及间隔2 来表示偶数
+        s +=1.0/i
+    return s
 
-
-    pass
 
 def test_jishu(n):
-    pass
+    i = 0
+    s = 0.0
+    for i in range(1, n + 1, 2):  # 初始值为1及间隔2 来表示奇数
+        s += 1.0 / i
+    return s
 
 def test(n):
     if n % 2 ==0:
-        test_oushu(n)
+        print(test_oushu(n))  #调用函数时，如果直接return的数据 还需要在print 一下才能显示出来
     else:
-        test_jishu(n)
+        print(test_jishu(n))
 
 
 if __name__ == '__main__':
-    n = int(input('请输入一个数'))
+    n = int(input('请输入一个数：'))
     test(n)
 
 
